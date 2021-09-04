bd18687378609476fae4f2887a79062441210db5
Harshit Garg <ggharshit1@gmail.com>
2021-09-04 21:04:26 +0530

    Add Songs Cover and Complete CoverFlowJS

---
+++ src/assets/songCover/coverflow.js
@@ -0,0 +1,26 @@
+import beheneDeImage from './behenedeImage.jpg'
+import mohmohkedaageImage from './mohmohkedaageImage.jpg'
+import orangrezImage from './orangrezImage.jpg'
+import phirleayadilImage from './phirleayadilImage.jpg'
+import phirseuddchalaImage from './phirseuddchalaImage.jpg'
+import terenainaImage from './terenainaImage.jpg'
+
+import beheneDeSong from '../songs/behenedeSong.mp3'
+import mohmohkedaageSong from '../songs/mohmohkedaageSong.mp3'
+import orangrezSong from '../songs/orangrezSong.mp3'
+import phirleayadilSong from '../songs/phirleayadilSong.mp3'
+import phirseuddchalaSong from '../songs/phirseuddchalaSong.mp3'
+import terenainaSong from '../songs/terenainaSong.mp3'
+
+const coverFlow = [
+
+    { _id: 0, name: "Behene De", images: beheneDeImage, src: beheneDeSong, artist: "Various Artist", album: "Raavan"},
+    { _id: 1, name: "Moh Moh Ke Dhaage", images: mohmohkedaageImage, src: mohmohkedaageSong, artist: "Various Artist", album: "Dum Laga Ke Haisha"},
+    { _id: 2, name: "O Rangrez", images: orangrezImage, src: orangrezSong, artist: "Various Artist", album: "Bhaag Milkha Bhaag"},
+    { _id: 3, name: "Phir Le Aya Dil", images: phirleayadilImage, src: phirleayadilSong, artist: "Various Artist", album: "Barfi"},
+    { _id: 4, name: "Phir Se Udd Chala", images: phirseuddchalaImage, src: phirseuddchalaSong, artist: "Various Artist", album: "Rockstar"},
+    { _id: 5, name: "Tere Naina", images: terenainaImage, src: terenainaSong, artist: "Various Artist", album: "My Name Is Khan"}
+
+]
+
+export default coverFlow;
