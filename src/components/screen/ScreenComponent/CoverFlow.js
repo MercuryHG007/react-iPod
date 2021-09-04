e2b9bb24b0257e7b13b784d6f6afdcb7f84f69d3
Harshit Garg <ggharshit1@gmail.com>
2021-09-04 21:38:33 +0530

    Complete CoverFlowJS Component

---
+++ src/components/screen/ScreenComponent/CoverFlow.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import NotificationBar from './NotificationBar';
+import Coverflow from 'react-coverflow';
+import coverflowData from '../../../assets/songCover/coverflow';
+import { AppContent } from '../../../context/playContext';
+
+function CoverFlow () {
+    const { activeState } = useContext(AppContent);
+
+    return(
+        <div>
+            <NotificationBar />
+            <Coverflow 
+                width={100}
+                height={235}
+                displayQuantityOfSide={1}
+                enableHeading={false}
+                actie={activeState}
+            >
+                {coverflowData.map((item) => (
+                    <img 
+                        key={item._id} 
+                        src={item.images} 
+                        alt={item.name}
+                    />
+                ))}
+            </Coverflow>
+        </div>
+    )
+}
+
+export default CoverFlow;
