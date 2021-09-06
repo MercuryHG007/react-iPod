7c455fb1dadb65f5c1ee3bb2e43714dc3c94b7f6
Harshit Garg <ggharshit1@gmail.com>
2021-09-06 10:39:34 +0530

    Complete AssetsJS

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import arrowright from './icons/arrowright.svg';
+import battery from './icons/battery.svg';
+import card from './icons/card.png';
+import fastforword from './icons/fastforword.svg';
+import gameIcon from './games/game.png';
+import highVolume from './icons/highVolume.svg';
+import lowVolume from './icons/lowVolume.svg';
+import ipodlogo from './icons/ipodlogo.svg';
+import menu from './icons/menu.svg';
+import pause from './icons/pause.svg';
+import play from './icons/play.svg';
+import playpause from './icons/playpause.svg';
+import reacticon from './icons/reacticon.png';
+import rewind from './icons/rewind.svg';
+
+const assets = {
+    arrowright,
+    battery,
+    card,
+    fastforword,
+    gameIcon,
+    highVolume,
+    lowVolume,
+    ipodlogo,
+    menu,
+    pause,
+    play,
+    playpause,
+    reacticon,
+    rewind
+};
+
+export default assets;
