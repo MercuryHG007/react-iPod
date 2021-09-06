234eff5aca441469a9d3046e5b0840d653a6853b
Harshit Garg <ggharshit1@gmail.com>
2021-09-06 10:54:04 +0530

    Complete SettingsJS Component

---
+++ src/components/screen/ScreenComponent/Settings.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import assets from '../../../assets/assets';
+import { AppContent } from '../../../context/playContext';
+import { settings } from '../../../data/data';
+
+function Settings () {
+    const { activeState } = useContext(AppContent);
+    return(
+        <div className="setting">
+            <div className="settingContainer">
+                <div>
+                    <img src={assets.ipodlogo} alt='' />
+                </div>
+                <div className="ipod">iPod.js</div>
+            </div>
+
+            <div className="text"> MADE BY HARSHIT GARG  </div>
+            {settings.map((item, index) => (
+                <div
+                    key={item.name}
+                    id='setting-item'
+                    className={index === activeState ? 'active' : ''}
+                >
+                    <a
+                        className="url"
+                        href={item.url}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        {item.name}
+                    </a>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Settings;
